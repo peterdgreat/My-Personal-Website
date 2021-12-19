8ed61ba856b3d11185adacdd5e333835b2b2747e
Ajayi Peter <peterdgreat2016@gmail.com>
2021-12-19 20:15:34 +0100

    See Projects Styled

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import '../styles/Projects.css'
 import { useSelector } from 'react-redux';
 import ImageList from '@mui/material/ImageList';
@@ -8,48 +8,68 @@ import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { useDispatch } from 'react-redux';
-import { getProjects } from '../redux/projects/projects'
-
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles({
+  spacing: {
+    margin: "14px !important",
+    gap: "14px",
+  },
+  seeProj : {
+    backgroundColor: "#f55800 !important",
+    minHeight:"37px !important",
+    cursor: "pointer",
+    textAlign: "center",
+  },
+  imgWrapper : {
+    backgroundColor: "#3E3C3C !important",
+  },
+  img : {
+    opacity : "0.1 !important",
+    transition : "all 0.5s ease-in-out",
+     "&:hover": {
+      opacity: "1 !important",
+    },
+  },
+  details : {
+    position: "absolute",
+     transition : "all 0.5s ease-in-out",
+     "&:hover": {
+      opacity: "0 !important",
+    },
+  }
+});
 export default function Project() {
-//       const dispatch = useDispatch();
-//   useEffect(() => {
-//     dispatch(getProjects());
-//   }, []);
+   const classes = useStyles();
        const projects = useSelector(state => state.projectsReducer.projects)
-        const matches = useMediaQuery('(min-width:1455px)');
+        const matches = useMediaQuery('(min-width:1400px)');
     console.log(projects)
   return (
       <div className="d-flex justify-content-center">
-    <ImageList  sx={{ width: 1200}}>
-
+    <ImageList  sx={{ width: 1200}} className={classes.spacing}>
+ 
       {matches && (<ImageListItem key="Subheader" cols={3}>
-   <ListSubheader component="div" className="text-center">My Projects</ListSubheader>
+  <ListSubheader component="div" className="text-center">My Projects</ListSubheader>
       </ImageListItem>)
           }
                
       
       {projects.map((project) => (
-        <ImageListItem key={project.id}>
+        <ImageListItem key={project.id} className= {classes.imgWrapper}>
            
    <img
             src={project.img}
             alt={project.title}
             loading="lazy"
+            className={classes.img}
           />
-         
        
+        {/* <div className={classes.details}>
+         <h3> {project.title}</h3>
+          <p>{project.details}</p>
+        </div> */}
           <ImageListItemBar
-            title={project.title}
-            subtitle='later'
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${project.title}`}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
+          className={classes.seeProj}
+            title={project.btn}
           />
         
         </ImageListItem>
@@ -58,77 +78,4 @@ export default function Project() {
  </div> );
 }
 
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
-    cols: 2,
-  },
-];
 
