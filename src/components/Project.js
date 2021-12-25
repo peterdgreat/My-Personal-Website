2748ad663cdcb917c911bc6f231225819110f786
Ajayi Peter <peterdgreat2016@gmail.com>
2021-12-25 02:17:16 +0100

    Animation effects added

--- src/components/Project.js
+++ src/components/Project.js
@@ -30,8 +30,12 @@ const useStyles = makeStyles({
   img: {
     opacity: '0.5 !important',
     transition: 'all 0.5s ease-in-out',
+
     '&:hover': {
       opacity: '1 !important',
+      animation: 'shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both',
+      transform: 'translate3d(0, 0, 0)',
+      perspective: '1000px',
     },
   },
   details: {
