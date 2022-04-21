0f299fd553b1f48c3df371b0700ab206465a257c
Ajayi Peter <peterdgreat2016@gmail.com>
2022-04-21 20:22:51 +0200

    added particles js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,92 @@
 import React from 'react';
 import '../styles/Hero.css';
-// import Particles from 'react-tsparticles';
-// import { loadFull } from 'tsparticles';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 import HeadShot from '../assets/slack.jpg';
 
 export default function Hero() {
-  // const particlesInit = async (main) => {
-  //   console.log(main);
-  //   await loadFull(main);
-  // };
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
 
-  // const particlesLoaded = (container) => {
-  //   console.log(container);
-  // };
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <div className="home-bg w-100 ">
-      {/* <Particles id="tsparticles" url="http://foo.bar/particles.json" init={particlesInit} loaded={particlesLoaded} /> */}
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 0,
+              },
+              repulse: {
+                distance: 500,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#ffffff',
+            },
+            links: {
+              color: '#ffffff',
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: false,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 3000,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <div className=" w-100 h-100 d-flex align-items-start justify-content-end home ">
         <section className="d-flex flex-md-column s-container">
           <section className=" d-flex justify-content-center w-100 align-items-center">
