3d4ed9f4779c4d82ad033ccb6b1aaa3a344511e8
Ajayi Peter <peterdgreat2016@gmail.com>
2022-04-21 18:40:37 +0200

    removed unused codes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,52 +1,21 @@
 import React from 'react';
 import '../styles/Hero.css';
-// import TwitterIcon from '@mui/icons-material/Twitter';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
-// import GitHubIcon from '@mui/icons-material/GitHub';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemIcon from '@mui/material/ListItemIcon';
-// import { makeStyles } from '@material-ui/core/styles';
-// import medium from '../assets/medium.svg';
+// import Particles from 'react-tsparticles';
+// import { loadFull } from 'tsparticles';
 import HeadShot from '../assets/slack.jpg';
 
-// const useStyles = makeStyles({
-//   icon: {
-//     color: '#fff !important',
-//     textDecoration: 'none',
-//     minWidth: '0 !important',
-//   },
-// });
-
 export default function Hero() {
-  // const classes = useStyles();
-  // const socialMedia = [
-  //   {
-  //     id: 1,
-  //     url: 'https://github.com/peterdgreat',
-  //     text: 'Github',
-  //     icon: <GitHubIcon />,
-  //   },
-  //   {
-  //     id: 2,
-  //     url: 'https://www.linkedin.com/in/ajayipeter/',
-  //     text: 'LinkedIn',
-  //     icon: <LinkedInIcon />,
-  //   },
-  //   {
-  //     id: 3,
-  //     url: 'https://twitter.com/dev_Peter_O',
-  //     text: 'Twitter',
-  //     icon: <TwitterIcon />,
-  //   },
-  //   {
-  //     id: 4,
-  //     url: 'https://medium.com/@peterdgreat',
-  //     text: 'Medium',
-  //     icon: <img className="icon mt-1" alt="medium" src={medium} />,
-  //   },
-  // ];
+  // const particlesInit = async (main) => {
+  //   console.log(main);
+  //   await loadFull(main);
+  // };
+
+  // const particlesLoaded = (container) => {
+  //   console.log(container);
+  // };
   return (
     <div className="home-bg w-100 ">
+      {/* <Particles id="tsparticles" url="http://foo.bar/particles.json" init={particlesInit} loaded={particlesLoaded} /> */}
       <div className=" w-100 h-100 d-flex align-items-start justify-content-end home ">
         <section className="d-flex flex-md-column s-container">
           <section className=" d-flex justify-content-center w-100 align-items-center">
@@ -54,13 +23,6 @@ export default function Hero() {
               <img className="headshot img-fluid" src={HeadShot} alt="headshot" />
             </div>
           </section>
-          {/* {socialMedia.map((link) => (
-            <ListItem button key={link.id}>
-              <a href={link.url}>
-                <ListItemIcon className={classes.icon}>{link.icon}</ListItemIcon>
-              </a>
-            </ListItem>
-          ))} */}
         </section>
         <section className="d-flex flex-column w-100 ">
           <div className="section m-container">
